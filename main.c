cde6f4b35be9e69181dda96c2b14cb3c70812dde
Ayman Touhami <atouhami@e1r1p3.1337.ma>
2022-01-02 19:07:43 +0100

    program is now taking inputs

--- main.c
+++ main.c
@@ -1,6 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atouhami <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/02 18:49:50 by atouhami          #+#    #+#             */
+/*   Updated: 2022/01/02 18:49:54 by atouhami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-int main(void)
+int	ft_atoi(const char *str)
 {
-	printf("philosophers");
+	int	i;
+	int	sign;
+	int	count;
+
+	i = 0;
+	sign = 1;
+	count = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\v'
+		|| str[i] == '\r' || str[i] == '\f' || str[i] == '\n')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		count = count * 10 + str[i] - 48;
+		i++;
+	}
+	return (count * sign);
+}
+
+int take_input(int argc,char *argv[], t_args *args)
+{
+	if (argc != 6 && argc != 5)
+		return(printf("Wrong Arguments\n"),1);
+		args -> must_eat = 0;
+	args -> nb_philo = ft_atoi(argv[1]);
+	args -> time_die = ft_atoi(argv[2]);
+	args -> time_eat = ft_atoi(argv[3]);
+	args -> time_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		args -> must_eat = ft_atoi(argv[5]);
+	if (args -> nb_philo == 0 || args -> time_die == 0 || args -> time_eat == 0 || args -> time_sleep == 0)
+		return(printf("Wrong Arguments\n"),1);
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	t_args args;
+
+	if (take_input(argc, argv, &args) == 1)
+		return 0;
+
+	printf("%d %d %d %d %d", args.nb_philo, args.time_die, args.time_eat, args.time_sleep, args.must_eat);
 }
--- philo.h
+++ philo.h
@@ -1,6 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atouhami <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/02 18:50:18 by atouhami          #+#    #+#             */
+/*   Updated: 2022/01/02 18:50:20 by atouhami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
-
 # include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdlib.h>
+
+typedef struct s_args
+{
+	int nb_philo;
+	int time_die;
+	int time_eat;
+	int time_sleep;
+	int must_eat;
+}	t_args;
 
 #endif
